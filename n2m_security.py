dab9595bbfd31a39407ddd3205095256cf784556
Arthur Vuillard <arthur.vuillard@gmail.com>
2013-10-14 14:37:03 +0200

    Put the nagios code to python-mk-livestatus

--- n2m_security.py
+++ n2m_security.py
@@ -15,63 +15,12 @@
 # this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import csv
-import socket
-
-
-class NagiosRequest(object):  # pragma: no cover
-    def __init__(self, conn, resource):
-        self._conn = conn
-        self._resource = resource
-        self._columns = []
-        self._filter = []
-
-    def call(self):
-        if self._columns:
-            return self._conn.call(str(self), self._columns)
-        return self._conn.call(str(self))
-
-    def __str__(self):
-        request = 'GET %s' % (self._resource)
-        if self._columns:
-            request += '\nColumns: %s' % (' '.join(self._columns))
-        if self._filter:
-            for filter_line in self._filter:
-                request += '\nFilter: %s' % (filter_line)
-        return request + '\n\n'
-
-    def columns(self, *args):
-        self._columns = args
-        return self
-
-    def filter(self, filter_str):
-        self._filter.append(filter_str)
-        return self
-
-
-class NagiosLivestatus(object):  # pragma: no cover
-    def __init__(self, hostname, port):
-        self.hostname = hostname
-        self.port = port
-
-    def __getattr__(self, name):
-        return NagiosRequest(self, name)
-
-    def call(self, request, columns=None):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.hostname, self.port))
-            s.send(request)
-            s.shutdown(socket.SHUT_WR)
-            csv_lines = csv.DictReader(s.makefile(), columns, delimiter=';')
-            return list(csv_lines)
-        finally:
-            s.close()
+from mk_livestatus import Query, NagiosSocket
 
 
 class SecurityUpdatesChecker(object):
     def __init__(self):
-        self.nagios = NagiosLivestatus('192.168.222.5', 6557)
+        self.nagios = NagiosSocket('192.168.222.5', 6557)
 
     def _nagios_request(self):
         request = self.nagios.services
